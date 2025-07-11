5117a943d1ea38ee7c685bde02a9d387ce0baa91
Suvam Samantray <suvam.workforce@gmail.com>
2025-07-11 14:38:15 +0530

    JS beginning

---
+++ script.js
@@ -0,0 +1,19 @@
+// document.getElementById("increase").onclick = function () {
+//     alert("hela reeee")
+// }
+
+
+// function increament() {
+//     console.log("hei gala reeee")
+// }
+
+
+function count() {
+    console.log(5)
+    console.log(4) 
+    console.log(3)
+    console.log(2)
+    console.log(1)
+}
+
+count()
